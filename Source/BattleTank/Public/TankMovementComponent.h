86b87b91bd8450104b17e44c51227d1f9d31e02a
Mstone8370 <mstone8370@gmail.com>
2020-04-23 16:40:49 +0900

    248. Finalising Your Class Code

--- Source/BattleTank/Public/TankMovementComponent.h
+++ Source/BattleTank/Public/TankMovementComponent.h
@@ -18,21 +18,21 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category=Setup)
+	UFUNCTION(BlueprintCallable, Category="Setup")
 	void Initialize(UTankTrack *LeftTrackToSet, UTankTrack *RightTrackToSet);
-
-	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 	
-	UFUNCTION(BlueprintCallable, Category=Input)
+	UFUNCTION(BlueprintCallable, Category="Input")
 	void IntendMoveForward(float Throw);
 
-	UFUNCTION(BlueprintCallable, Category=Input)
+	UFUNCTION(BlueprintCallable, Category="Input")
 	void IntendTurnRight(float Throw);
 
-	UFUNCTION(BlueprintCallable, Category=Input)
+	UFUNCTION(BlueprintCallable, Category="Input")
 	void IntendTurnLeft(float Throw);
 
 private:
+	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
+
 	UTankTrack *LeftTrack = nullptr;
 	UTankTrack *RightTrack = nullptr;
 };
